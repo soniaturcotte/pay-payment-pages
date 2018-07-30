a0537c3cd34f78efc779006718466ef889ab1b75
sonia.turcotte@gmail.com <sonia.turcotte@gmail.com>
2018-07-30 16:27:27 +0100

    Update service name

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Example service name',
+  serviceName: 'Vehicle tax',
 
   // Default port that prototype runs on
   port: '3000',
